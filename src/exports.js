4ef05e1285b5ff73202b4965121abc8a468c7851
arran <ads@localhost>
2018-07-20 11:32:44 +0800

    don't repeat things in lib/index.js

--- src/exports.js
+++ src/exports.js
@@ -1,20 +1,8 @@
 
-const proptypes   = require('../lib/proptypes');
-const propgrammar = require('../lib/propgrammar');
-const nearley     = require('nearley');
+const prop_parser = require('../lib/index.js');
 
-function parse (str) {
-  var p = new nearley.Parser(nearley.Grammar.fromCompiled(propgrammar))
-  p.feed(str)
-  var res = p.results
-  if (res.length == 0) {
-    throw 'Need more input - did you forget to finish the proposition?'
-  }
-  // console.log("no. of parse trees: " + res.length);
-  return res[0]
-}
 
-global.parse     = parse
-global.proptypes = proptypes
-global.asciify   = proptypes.asciify
+global.parse     = prop_parser.parse
+global.proptypes = prop_parser.proptypes
+global.asciify   = prop_parser.proptypes.asciify
 
